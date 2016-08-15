b9843bcf70cc5f9e8c64d89e3e02992efe047507
StackMachine <robot@stackmachine.com>
2016-08-15 04:32:18 +0000

    Update to version 0.0.1471235537

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1471235460',
+    version='0.0.1471235537',
 
     description='GRPC Hello World client and server',
 
