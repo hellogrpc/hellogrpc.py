b806221190dca8b1a8c7ef9ec68ee72caeccb4f9
StackMachine <robot@stackmachine.com>
2016-08-24 04:22:13 +0000

    Update to version 0.0.1472012531

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1471414830',
+    version='0.0.1472012531',
 
     description='GRPC Hello World client and server',
 
@@ -58,5 +58,5 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['grpcio==1.0.0rc2']
+    install_requires=['grpcio==1.0.0']
 )
